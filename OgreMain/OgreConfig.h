1a7b7d27bfca638eeeb27d1ea15b3434694f8da6
InVOkERr <invokerleve@gmail.com>
2019-03-28 23:32:13 +0800

    feat：complete ogre export, convert file encode to utf-8

--- OgreMain/OgreConfig.h
+++ OgreMain/OgreConfig.h
@@ -1,31 +1,1 @@
-#ifndef __Config_H_
-#define __Config_H_
-
-#include "OgreBuildSttings.h"
-
-/** If set to >0, OGRE will always 'think' that the graphics card only has the
-    number of texture units specified. Very useful for testing multipass fallback.
-*/
-#define OGRE_PRETEND_TEXTURE_UNITS 0
-
-/** Define number of texture coordinate sets allowed per vertex.
-*/
-#define OGRE_MAX_TEXTURE_COORDS_SETS 8
-
-/** Define max number of texture layers allowed per pass on any card
-*/
-#define OGRE_MAX_TEXTURE_LAYERS 16
-
-/** Define max number of lights allowed per pass.
-*/
-#define OGRE_MAX_SIMULANEOUS_LIGHTS 8
-
-/** Define max number of blending weights allowed per vertex.
-*/
-#define OGRE_MAX_BLEND_WEIGHTS 4
-
-/** Define max number of the multiple render targets (MRTs) to render to at once.
-*/
-#define OGRE_MAX_MULITPLE_RENDER_TARGETS 8
-
-#endif
+#ifndef __Config_H_#define __Config_H_#include "OgreBuildSettings.h"/** If set to >0, OGRE will always 'think' that the graphics card only has the    number of texture units specified. Very useful for testing multipass fallback.*/#define OGRE_PRETEND_TEXTURE_UNITS 0/** Define number of texture coordinate sets allowed per vertex.*/#define OGRE_MAX_TEXTURE_COORDS_SETS 8/** Define max number of texture layers allowed per pass on any card*/#define OGRE_MAX_TEXTURE_LAYERS 16/** Define max number of lights allowed per pass.*/#define OGRE_MAX_SIMULANEOUS_LIGHTS 8/** Define max number of blending weights allowed per vertex.*/#define OGRE_MAX_BLEND_WEIGHTS 4/** Define max number of the multiple render targets (MRTs) to render to at once.*/#define OGRE_MAX_MULITPLE_RENDER_TARGETS 8#endif	// #ifndef __Config_H_
--- OgreMain/OgreExports.h
+++ OgreMain/OgreExports.h
@@ -1,42 +1,1 @@
-#ifndef _OgreExport_H_
-#define _OgreExport_H_
-
-#ifdef OGREMAIN_STATIC_DEFINE
-#  define _Ogre_EXPORT
-#  define _OgrePrivate
-
-#else
-#  ifndef _Ogre_Export
-#    ifdef OGREMAIN_EXPORTS
-        /* We are building this library */
-#       define _OgreExport __declspec(dllexport)
-#    else
-        /* We are using this library */
-#       define _Ogre_Export __declpec(dllimport)
-#    endif
-#  endif
-
-#  ifndef __OgrePrivate
-#    define __OgrePrivate
-#  endif
-#endif
-
-#ifndef OGRE_DEPRECATED
-#  define OGRE_DEPRECATED __declspec(deprecated)
-#endif
-
-#ifndef OGRE_DEPRECATED_EXPORT
-#  define OGRE_DEPRECATED_EXPORT _OgreExport OGRE_DEPRECATED
-#endif
-
-#ifndef OGRE_DEPRECATED_NO_EXPRECATED
-#  define OGRE_DEPRECATED_NO_EXPORT _OgrePrivate OGRE_DEPRECATED
-#endif
-
-#if 0 /* DEFINE_NO_DEPRECATED */
-#  ifndef OGREMAIN_NO_DEPRECATED
-#    define OGREMAIN_NO_DEPRECATED
-#  endif
-#endif
-
-#endif
+#ifndef _OgreExport_H_#define _OgreExport_H_#ifdef OGREMAIN_STATIC_DEFINE#  define _Ogre_EXPORT#  define _OgrePrivate#else#  ifndef _Ogre_Export#    ifdef OGREMAIN_EXPORTS        /* We are building this library */#       define _OgreExport __declspec(dllexport)#    else        /* We are using this library */#       define _Ogre_Export __declpec(dllimport)#    endif#  endif#  ifndef __OgrePrivate#    define __OgrePrivate#  endif#endif#ifndef OGRE_DEPRECATED#  define OGRE_DEPRECATED __declspec(deprecated)#endif#ifndef OGRE_DEPRECATED_EXPORT#  define OGRE_DEPRECATED_EXPORT _OgreExport OGRE_DEPRECATED#endif#ifndef OGRE_DEPRECATED_NO_EXPRECATED#  define OGRE_DEPRECATED_NO_EXPORT _OgrePrivate OGRE_DEPRECATED#endif#if 0 /* DEFINE_NO_DEPRECATED */#  ifndef OGREMAIN_NO_DEPRECATED#    define OGREMAIN_NO_DEPRECATED#  endif#endif#endif	// #ifndef _OgreExport_H_
